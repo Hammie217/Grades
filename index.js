fd930650722062743dcd340a2ec48082692398e8
Katie Walker <klwalker2@sheffield.ac.uk>
2018-07-29 18:51:23 +0100

    Started grades page

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, (req, res) => {
+app.listen(8080, () => {
     console.log('listening on 8080 snitch');
 });
 
-app.get('/', (req,res) => {
-    res.render(__dirname + "/public/index.ejs");
+app.get('/', () => {
+
+});
+
+app.get('/grades', (req, res) => {
+    res.render(__dirname + "/public/grades.ejs");
+});
+
+app.get('/db', (req, res) => {
+    
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "katie",
+        password: "password"
+      });
+
+      var printDb = ((err) => {
+
+      });
 });
