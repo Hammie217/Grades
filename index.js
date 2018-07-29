ec9108e982df0929833d5d9f3dd87eb34a0a7bf3
Katie Walker <klwalker2@sheffield.ac.uk>
2018-07-29 18:09:10 +0100

    Testing user

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, (req, res) => {
-    console.log('listening on 8080 bitch');
+    console.log('listening on 8080 snitch');
 });
 
 app.get('/', (req,res) => {
